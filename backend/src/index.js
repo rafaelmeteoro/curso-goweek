52f51413a58a9fef3a4384fbb3bd729b0786faeb
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-11-10 00:41:39 -0200

    save and update tweet

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
+mongoose.connect(
+    'mongodb://goweek:goweek123@ds049651.mlab.com:49651/goweek-backend', 
+    {
+        useNewUrlParser: true 
+    }
+);
+
+app.use(express.json());
+app.use(require('./routes'));
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
