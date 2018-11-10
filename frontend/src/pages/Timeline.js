b368c43bedee3a8f1c0bd09bfb9b17533bae08e2
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2018-11-10 18:57:55 -0200

    finish font web

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,22 +1,50 @@
 import React, { Component } from 'react';
+import api from '../services/api';
+import socket from 'socket.io-client';
 
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
+import Tweet from '../components/Tweet';
+
 export default class Timeline extends Component {
 
     state = {
+        tweets: [],
         newTweet: ''
     };
 
-    handleNewTweet = (e) => {
+    async componentDidMount() {
+        this.subscribeToEvents();
+
+        const response = await api.get('tweets');
+
+        this.setState({ tweets: response.data });
+    };
+
+    subscribeToEvents = () => {
+        const io = socket('http://localhost:3000');
+
+        io.on('tweet', data => {
+            this.setState({ tweets: [data, ...this.state.tweets] });
+        });
+
+        io.on('like', data => {
+            this.setState({ tweets: this.state.tweets.map(tweet =>
+                tweet._id === data._id ? data : tweet
+            )});
+        });
+    };
+
+    handleNewTweet = async (e) => {
         if (e.keyCode !== 13) return;
 
         const content = this.state.newTweet;
         const author = localStorage.getItem('@GoTwitter:username');
 
-        console.log(content, author);
-        
+        await api.post('tweets', { content, author });
+
+        this.setState({ newTweet: '' });
     };
 
     handleInputChange = (e) => {
@@ -36,6 +64,14 @@ export default class Timeline extends Component {
                         placeholder='O que está acontecendo?'
                     />
                 </form>
+
+                <ul className='tweet-list'>
+                    {
+                        this.state.tweets.map(tweet => (
+                            <Tweet key={tweet._id} tweet={tweet} />
+                        ))
+                    }
+                </ul>
             </div>
         );
     }
